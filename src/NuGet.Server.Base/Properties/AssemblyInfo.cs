0688661fe0fcc54d0234c76d729973bc3e8299d5
andulv <anders@ulveseth.net>
2016-05-31 23:19:45 +0200

    Added NuGet.Server.Base project

---
+++ src/NuGet.Server.Base/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("NuGet.Server.Base")]
+[assembly: AssemblyDescription("Class library used to host a simple NuGet feed")]
+[assembly: InternalsVisibleTo("NuGet.Server.Tests")]
